9cb4347c9563c052f85ef187172ee536edc5dc83
pravadaR <pravada290407@gmail.com>
2026-03-16 21:05:37 +0530

    Update lpp.py

--- lpp.py
+++ lpp.py
@@ -1,68 +1,68 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np #used for mathematical operations and arrays
+import matplotlib.pyplot as plt #used for drawing graphs
  
-print("Objective Function: Z = c1*x + c2*y") 
+print("Objective Function: Z = c1*x + c2*y") #displays obj dunction format to the user.
  
 c1 = int(input("Enter c1: ")) 
-c2 = int(input("Enter c2: ")) 
-constraints = [] 
-n = int(input("\nEnter number of constraints: ")) 
-for i in range(n): 
+c2 = int(input("Enter c2: ")) #user inputs the coefficients.
+constraints = []  #(a,b,sign,c)
+n = int(input("\nEnter number of constraints: ")) #user enters number of constraints.
+for i in range(n): #loop n times to take each constraint
   print("\nConstraint", i+1) 
   a = int(input("Enter a: ")) 
   b = int(input("Enter b: ")) 
-  sign = input("Enter sign (<= or >=): ") 
+  sign = input("Enter sign (<= or >=): ") #ineuality sign
   c = int(input("Enter c: ")) 
-  constraints.append((a, b, sign, c)) 
-x = np.linspace(0, 50, 400) 
-lines = [] 
-plt.figure() 
-for a, b, sign, c in constraints: 
-  if b != 0: 
-    y = (c - a*x)/b 
-    plt.plot(x, y) 
-  lines.append((a, b, c)) 
+  constraints.append((a, b, sign, c)) #store constraint
+x = np.linspace(0, 50, 400)  #create X values for graph creates 400 values of x b/w 0 and 50.
+lines = []  # stores equations for later intersection calculations.
+plt.figure() #starts a new plot figure
+for a, b, sign, c in constraints: #loop through each constraint
+  if b != 0: #avoid division by zero
+    y = (c - a*x)/b #convert eq to y form
+    plt.plot(x, y) #draws the constraint line on graph
+  lines.append((a, b, c)) #stores eq for intersection calculations.
  
 lower = np.zeros(len(x)) 
-upper = np.ones(len(x)) * 50 
+upper = np.ones(len(x)) * 50 #used for shading feasible region.
  
 for a, b, sign, c in constraints: 
    if b != 0: 
        y = (c - a*x)/b 
        if sign == "<=": 
-           upper = np.minimum(upper, y) 
+           upper = np.minimum(upper, y) #feasible region lies below line.
        else: 
-           lower = np.maximum(lower, y) 
+           lower = np.maximum(lower, y) #feasible region lines above line.
  
-plt.fill_between(x, lower, upper, where=(upper >= lower), alpha=0.3) 
+plt.fill_between(x, lower, upper, where=(upper >= lower), alpha=0.3) #shades feasible area.
  
-def feasible(xp, yp): 
+def feasible(xp, yp): #checks whether a point satisfies all constraints.
    for a, b, sign, c in constraints: 
-       val = a*xp + b*yp 
+       val = a*xp + b*yp #substitute point into equation
        if sign == "<=" and val > c: 
-           return False 
+           return False #if violated->not feasible
        if sign == ">=" and val < c: 
-           return False 
+           return False #point is feasible
  
-   return True 
+   return True #point is feasible
  
-corner = [] 
+corner = [] #stores all feasible intersection points
  
-for i in range(len(lines)): 
+for i in range(len(lines)): #loop through first line.
    a1, b1, c1_ = lines[i] 
-   for j in range(i+1, len(lines)): 
+   for j in range(i+1, len(lines)): #find inersections between lines
        a2, b2, c2_ = lines[j] 
        det = a1*b2 - a2*b1 
        if det != 0:   # not parallel 
            x_int = (c1_*b2 - c2_*b1) / det 
            y_int = (a1*c2_ - a2*c1_) / det 
-           if x_int >= 0 and y_int >= 0: 
-               if feasible(x_int, y_int): 
-                   corner.append((x_int, y_int)) 
+           if x_int >= 0 and y_int >= 0: #check first quadrant
+               if feasible(x_int, y_int): #only keep valid points
+                   corner.append((x_int, y_int)) #store corner point
  
 for a, b, c in lines: 
    if a != 0: 
-       x_int = c/a 
+       x_int = c/a #check axis 
        if x_int >= 0 and feasible(x_int, 0): 
  
            corner.append((x_int, 0)) 
@@ -71,24 +71,23 @@ for a, b, c in lines:
        if y_int >= 0 and feasible(0, y_int): 
            corner.append((0, y_int)) 
  
-if len(corner) == 0: 
+if len(corner) == 0: #if no feasible region
    print("\nNo feasible solution") 
    plt.show() 
    exit() 
  
-Zmax = None 
-best_points = [] 
+Zmax = None # initialise max Z
+best_points = [] #stores optimal value and points
  
 print("\n") 
  
-for xp, yp in corner: 
-   Z = c1*xp + c2*yp 
-   print("(" + str(round(xp,2)) + "," + str(round(yp,2)) + ") ->", round(Z,2)) 
+for xp, yp in corner: #loop through corner points
+   Z = c1*xp + c2*yp #objective func
+   print("(" + str(round(xp,2)) + "," + str(round(yp,2)) + ") ->", round(Z,2)) #print result
  
-   plt.scatter(xp, yp, color="red") 
+   plt.scatter(xp, yp, color="red") #plot corner point
  
-   plt.text(xp+0.2, yp+0.2, "(" + str(round(xp,1)) + "," + str(round(yp,1)) + 
-")") 
+   plt.text(xp+0.2, yp+0.2, "(" + str(round(xp,1)) + "," + str(round(yp,1)) + ")  #label point
  
    if Zmax is None or Z > Zmax: 
        Zmax = Z 
